c55cf78e744b58cbc976612fd4b01ed48aabaed1
Matt Olsen <olsen_matthew@yahoo.com>
2016-09-02 22:47:57 -0700

    brute forced method

--- app.js
+++ app.js
@@ -3,7 +3,9 @@
 let arr = [6,7,9,9];
 
 const incrementArrV1 = input => {
-  let numFromArr = parseInt(input.join(''));
+  let numFromArr = input.join('');
+  numFromArr++;
+  return numFromArr.toString().split('');
 };
 
 incrementArrV1(arr);
